669fe116b9d2fe2a76ddc83b399c1f81a03ea625
johnlouisebergabena <johnlouisebergs123@gmail.com>
2025-01-30 15:52:37 +0800

    Lobot sa Iro

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './assets/components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello, World!</h1>
+    </>
+  );
+}
+
+export default App;
